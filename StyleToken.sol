af55dcbac18a95d7ef77910484b30392ae87ea8a
Brewdogg1 <conmerging@gmail.com>
2024-11-02 17:03:23 -0700

    Add files via upload

--- StyleToken.sol
+++ StyleToken.sol
@@ -2,10 +2,10 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.22;
 
-import "@openzeppelin/contracts@5.1.0/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts@5.1.0/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts@5.1.0/access/Ownable.sol";
-import "@openzeppelin/contracts@5.1.0/token/ERC20/extensions/ERC20FlashMint.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20FlashMint.sol";
 
 /// @custom:security-contact conmerging@gmail.com
 contract LifeStylistic is ERC20, ERC20Burnable, Ownable, ERC20FlashMint {
@@ -18,4 +18,3 @@ contract LifeStylistic is ERC20, ERC20Burnable, Ownable, ERC20FlashMint {
         _mint(to, amount);
     }
 }
-
